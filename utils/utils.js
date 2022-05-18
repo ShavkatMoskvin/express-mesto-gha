75c2626483aff81bcef6c135263921aeb24c038a
ShavkatMoskvin <Shavkatmoskvin@gmail.com>
2022-05-18 22:39:05 +0300

    .

--- utils/utils.js
+++ utils/utils.js
@@ -1,5 +1,6 @@
 /* eslint-disable linebreak-style */
 module.exports.validateUrl = (url) => {
+  // eslint-disable-next-line no-useless-escape
   const regex = /^https?:\/\/(www\.)?[a-zA-Z\d]+\.[\w\-._~:\/?#[\]@!$&'()*+,;=]{2,}#?$/g;
   if (regex.test(url)) {
     return url;
